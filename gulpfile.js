35e3827abd596674f9ee3276b2ae3f750c05b7b4
Daniel Röhers Moura <rohersmoura@gmail.com>
2017-10-10 00:49:10 -0300

    migrate express

--- gulpfile.js
+++ gulpfile.js
@@ -3,12 +3,15 @@ const del = require('del');
 const babel = require('gulp-babel');
 const uglify = require('gulp-uglify');
 const cleanCSS = require('gulp-clean-css');
-const htmlmin = require('gulp-htmlmin');
 const swPrecache = require('sw-precache');
 
 const paths = {
-  dist: 'dist',
-  src: 'src',
+  dist: 'public',
+  assets: {
+    js: 'app/assets/js',
+    css: 'app/assets/css',
+    images: 'app/assets/images'
+  },
   config: 'config'
 };
 
@@ -17,24 +20,16 @@ gulp.task('del', () => {
 });
 
 gulp.task('minify-js', () => {
-  return gulp.src(`${paths.src}/*.js`)
+  return gulp.src(`${paths.assets.js}/**.js`)
     .pipe(babel())
     .pipe(uglify())
-    .pipe(gulp.dest(paths.dist));
+    .pipe(gulp.dest(`${paths.dist}/js`));
 });
 
 gulp.task('minify-css', () => {
-  return gulp.src(`${paths.src}/*.css`)
+  return gulp.src(`${paths.assets.css}/**.css`)
     .pipe(cleanCSS())
-    .pipe(gulp.dest(paths.dist));
-});
-
-gulp.task('minify-html', () => {
-  return gulp.src(`${paths.src}/*.html`)
-    .pipe(htmlmin({
-      collapseWhitespace: true
-    }))
-    .pipe(gulp.dest(paths.dist));
+    .pipe(gulp.dest(`${paths.dist}/css`));
 });
 
 gulp.task('config', () => {
@@ -44,8 +39,8 @@ gulp.task('config', () => {
 
 gulp.task('generate-service-worker', (cb) => {
   swPrecache.write(`${paths.dist}/service-worker.js`, {
-    staticFileGlobs: [paths.src + '/**/*.{js,html,css,png,jpg,gif,svg,eot,ttf,woff}'],
-    stripPrefix: paths.src,
+    staticFileGlobs: ['/public/**/*.{js,css,png,jpg,gif,svg,eot,ttf,woff}'],
+    stripPrefix: '/public',
     replacePrefix: '/'
   }, cb);
 });
@@ -54,7 +49,6 @@ gulp.task('default', [
   'del',
   'minify-js',
   'minify-css',
-  'minify-html',
   'generate-service-worker',
   'config'
 ]);
